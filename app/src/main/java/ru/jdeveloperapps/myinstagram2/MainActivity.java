9e81f1c0f29d6c886c838e57ec70260b2cd99fe4
Jvitaliy82 <jvit82@gmail.com>
2019-09-24 22:20:16 +0700

    init for lesson 4

--- app/src/main/java/ru/jdeveloperapps/myinstagram2/MainActivity.java
+++ app/src/main/java/ru/jdeveloperapps/myinstagram2/MainActivity.java
@@ -4,33 +4,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "myLog";
 
-    private Button logIn;
-    private Button close;
-    private FloatingActionButton fab;
-    private BottomSheetBehavior bottomSheetBehavior;
-
-    private View.OnClickListener snackbarOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            // Действия
-            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,46 +21,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-//                R.string.open_drawer,
-//                R.string.close_drawer);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
-        fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        initUi();
-    }
-
-    private void initUi() {
-        logIn = findViewById(R.id.btn_login);
-        close = findViewById(R.id.btn_close);
-
-        logIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar mSnackbar = Snackbar.make(fab, "Доступ ограничен!", Snackbar.LENGTH_LONG);
-                mSnackbar.setAction("Открыть", snackbarOnClickListener);
-                mSnackbar.show();
-            }
-        });
-
-        close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-        LinearLayout llBottomSheet = findViewById(R.id.bottom_sheet);
-        bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.open_drawer,
+                R.string.close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
